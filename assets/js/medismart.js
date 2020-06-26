70b5f2d31307aacb6e177d8a7cda19b401c973b0
Bastian <bstnbas3@gmail.com>
2020-06-26 13:12:30 -0400

    Cambios varios de diseño

--- assets/js/medismart.js
+++ assets/js/medismart.js
@@ -30,3 +30,8 @@ document.getElementById("video-testimonio").currentTime = 0;
 $('#exampleModal').on('shown.bs.modal', function (e) {
 document.getElementById("video-testimonio").play()
 })
+
+/* Animaciones */
+function cubica_Bezier(p0, p1, p2, p3, t) {
+    return p0*(1 - t)**3 + 3*p1*t*(1 - t)**2 + 3*p2*(t**2)*(1 - t) + p3*(t**3)*(1-t)
+}
