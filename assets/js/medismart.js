ca885f58f9bf86ca1edd773c3bef608d92f08aee
Bastian <bstnbas3@gmail.com>
2020-07-02 21:09:53 -0400

    Cambios en responsive, pequeños cambios en textos de diferentes pestañas

--- assets/js/medismart.js
+++ assets/js/medismart.js
@@ -77,13 +77,12 @@ $(document).ready(function (){
             }
         })
     }
-        $.ajax({
-            url: urlContenido,
-            dataType: "html",
-            success: function(data){
-                document.getElementById("footer-script").innerHTML = data;
-            }
-        })
-    }
+    $.ajax({
+        url: urlContenido,
+        dataType: "html",
+        success: function(data){
+            document.getElementById("footer-script").innerHTML = data;
+        }
+    })
 });
     
